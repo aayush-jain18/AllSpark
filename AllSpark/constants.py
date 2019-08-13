46c1d85fc24b40ddf209f12f37fe2523f6e297a8
aayush-jain18 <aayush.jain18@gmail.com>
2019-08-13 14:41:34 +0530

    #4 Immplement the output method

--- AllSpark/constants.py
+++ AllSpark/constants.py
@@ -2,6 +2,9 @@ class Constants:
     LOG_FORMAT = '%(asctime)-15s - %(levelname)s - %(funcName)s: %(message)s'
     IF_DUPLICATE_KEY_METHODS = ('drop', 'sort', 'aggregate')
     IF_DUPLICATE_ROWS = ('drop', None)
-    METADATA_COLUMNS = ['column', 'left_column','left_count', 'right_column', 'right_count', 'diff_column', 'diff_count']
+    METADATA_COLUMNS = ['column', 'column_present', 'left_column', 'left_count',
+                        'right_column', 'right_count', 'diff_column',
+                        'diff_count']
+    DIFF_TRUE = 'AllSpark.Compare.True'
 
 
